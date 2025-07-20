74fa23923a3bcbe70411139345b6cdc060c0f9bd
Wayne Wright <waynezilla@hotmail.co.uk>
2025-07-20 10:35:23 +0100

    FORCE VERCEL: Add deployment comment to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
-import '../styles/globals.css';
+// FORCE VERCEL DEPLOYMENT - Latest commit: 38a31b2 - Fixed PortfolioCompilationService and added evidence status refresh
 import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
